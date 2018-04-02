041dbec25e1106bdce5478ea8df319a3987c7e87
“lvyangzhuo <lvyangzhuo@163.com>
2018-04-02 12:48:15 +0800

    实现点击交互组件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ClickCounter from "./ClickCounter";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ClickCounter />, document.getElementById('root'));
 registerServiceWorker();
